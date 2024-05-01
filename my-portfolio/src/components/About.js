61a117a398d7736f14314fc4890314adba6c3e2f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 00:34:25 +0200

    Create About.js

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/About.css';
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>Welcome to my portfolio! I'm a passionate developer with a keen interest in building innovative web applications.</p>
+      {/* Add more content about yourself */}
+    </section>
+  );
+}
+
+export default About;
