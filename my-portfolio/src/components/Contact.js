fa497ae2c843bf54e803be6681178dbd711e6a2d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 00:37:53 +0200

    Create Contact.js

---
+++ my-portfolio/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Contact.css';
+
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact</h2>
+      {/* Add your contact information form here */}
+    </section>
+  );
+}
+
+export default Contact;
