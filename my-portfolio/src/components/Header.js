7a88268246838fdba925242fea561edca3ac8d04
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 01:31:02 +0200

    Update Header.js

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -3,9 +3,8 @@ import './styles/Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <h1>My Portfolio</h1>
-      <nav>
+    <header>
+      <nav className="navbar">
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
