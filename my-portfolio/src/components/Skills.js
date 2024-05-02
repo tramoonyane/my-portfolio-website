a02fef611a13320ae12820f870dc164f17d7c9c8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 03:35:28 +0200

    Update Skills.js

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -4,10 +4,7 @@ import '../styles/Skills.css';
 function Skills() {
   return (
     <section id="skills">
-      <div className="skills-container">
-        <h2>Skills</h2>
-        {/* Add skills and experience content here */}
-      </div>
+      {/* Your Skills content */}
     </section>
   );
 }
