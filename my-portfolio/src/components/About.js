02c0a0eb8ad3fd3604d08655749d4d1046aa5986
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 04:38:58 +0200

    Update About.js

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -4,7 +4,11 @@ import '../styles/About.css';
 function About() {
   return (
     <section id="about">
-      {/* Your About content */}
+    <div className="container">
+    <h2>About Me</h2>
+    <p>I am Thetele Ramoonyane, a passionate Software engineer based in Lesotho, Maseru. I have a background in Software engineering and specialize in Frontend and Backend development.</p>
+    <p>I love Machine learning, Augmented and virtual reality, and I'm always eager to learn and explore new technologies.</p>
+    </div>
     </section>
   );
 }
