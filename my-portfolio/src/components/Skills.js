3b16c07e89476db9e715414dc21653eaa34457dc
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 14:39:00 +0200

    Update Skills.js

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -4,17 +4,20 @@ import '../styles/Skills.css';
 function Skills() {
   return (
     <section id="skills">
-    <div className="container">
-    <h2>Skills</h2>
-    <ul>
-    <li>JavaScript</li>
-    <li>React</li>
-    <li>HTML5/CSS3</li>
-    <li>Node.js</li>
-    <li>Git</li>
-    {/* Add more skills as needed */}
-  </ul>
-    </div>
+      <div className="container">
+        <h2>Skills</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>HTML5/CSS3</li>
+          <li>Node.js</li>
+          <li>Git</li>
+          <li>NoSQL</li> {/* New skill: NoSQL */}
+          <li>Advanced SQL</li> {/* New skill: Advanced SQL */}
+          <li>Introduction to MySQL</li> {/* New skill: Introduction to MySQL */}
+          {/* Add more skills as needed */}
+        </ul>
+      </div>
     </section>
   );
 }
