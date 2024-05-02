e6d365ff304c39491c601e9c0ba35e7c162c16c2
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 04:46:05 +0200

    Update Projects.js

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -4,7 +4,18 @@ import '../styles/Projects.css';
 function Projects() {
   return (
     <section id="projects">
-      {/* Your Projects content */}
+    <div className="container">
+    <h2>Projects</h2>
+    <div className="project">
+    <h3>Project 1</h3>
+    <p>Description of Project 1</p>
+    </div>
+    <div className="project">
+    <h3>Project 2</h3>
+    <p>Description of Project 2</p>
+    </div>
+    {/* Add more projects as needed */}
+  </div>
     </section>
   );
 }
