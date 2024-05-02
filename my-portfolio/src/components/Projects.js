7b7eb70e55c2af357dff50aea8c9c9abcea87033
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 03:33:55 +0200

    Update Projects.js

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../styles/Projects.css';
 
-function Projects() {
+unction Projects() {
   return (
     <section id="projects">
-      <div className="projects-container">
-        <h2>Projects</h2>
-        {/* Add project cards or tiles here */}
-      </div>
+      {/* Your Projects content */}
     </section>
   );
 }
