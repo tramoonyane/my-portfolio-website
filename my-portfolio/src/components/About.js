fee96911fc20cb4ab975e438401544855d3e101e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 03:31:28 +0200

    Update About.js

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -4,11 +4,7 @@ import '../styles/About.css';
 function About() {
   return (
     <section id="about">
-      <div className="about-container">
-        <h2>About Me</h2>
-        <p>Welcome to my portfolio! I'm a passionate developer with a keen interest in building innovative web applications.</p>
-        {/* Add more content about yourself */}
-      </div>
+      {/* Your About content */}
     </section>
   );
 }
