9f4da556e72bd942080ec879c976757bbf56bc47
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 03:36:39 +0200

    Update Contact.js

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -4,10 +4,7 @@ import '../styles/Contact.css';
 function Contact() {
   return (
     <section id="contact">
-      <div className="contact-container">
-        <h2>Contact</h2>
-        {/* Add contact form or contact information here */}
-      </div>
+      {/* Your Contact content */}
     </section>
   );
 }
