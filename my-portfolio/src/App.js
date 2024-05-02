958acddf63e36c8e572b84d0187bddbce433e012
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-02 02:34:40 +0200

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,7 +5,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
-import './styles/App.css';
+import './App.css';
 
 function App() {
   return (
