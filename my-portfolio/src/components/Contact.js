f4ad90ca3812df5d08a42bc0dff1be1be0b4eb12
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-05-03 12:36:50 +0200

    Update Contact.js

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -4,15 +4,20 @@ import '../styles/Contact.css';
 function Contact() {
   return (
     <section id="contact">
-    <div className="container">
-    <h2>Contact Me</h2>
-    <p>Feel free to reach out to me via email or connect with me on social media.</p>
-    <ul>
-    <li>Email: theteleramoonyane5@gmail.com</li>
-    <li>LinkedIn: <a href="https://www.linkedin.com/">https://www.linkedin.com/in/thetele-ramoonyane-319488120/?originalSubdomain=ls</a></li>
-  {/* Add more contact options as needed */}
-  </ul>
-    </div>
+      <div className="container">
+        <h2>Contact Me</h2>
+        <p>Feel free to reach out to me via email or connect with me on social media.</p>
+        <ul>
+          <li>Email: theteleramoonyane5@gmail.com</li>
+          <li>
+            LinkedIn: 
+            <a href="https://www.linkedin.com/">
+              https://www.linkedin.com/in/thetele-ramoonyane-319488120/?originalSubdomain=ls
+            </a>
+          </li>
+          {/* Add more contact options as needed */}
+        </ul>
+      </div>
     </section>
   );
 }
